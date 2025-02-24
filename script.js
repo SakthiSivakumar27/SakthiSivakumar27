a1fb8bc627d1901dba569d98668fab731f3b1fc1
SakthiSivakumar27 <sakthisivakumar027@gmail.com>
2025-02-25 00:10:40 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function redirectToIndex(event) {
     event.preventDefault(); // Prevents default form submission
-    window.location.href = "index.html"; // Redirect to index page
+    window.location.href = "login.html"; // Redirect to index page
 }
